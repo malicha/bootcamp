c4bca8ee2d732aa6e8d48d35cde2f3022ffc0dc9
malicha <maalicha.maalicha@gmail.com>
2019-01-12 19:11:38 +0700

    object

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-const interestRate = 0.5;
-interestRate = 2;
-console.log(interestRate)
+let nama = "malicha";
+let umur = 18
+let person ={
+    nama: "malicha",
+    umur: 18
+}
+
+person.nama = "laela";
+
+let selection = "nama"
+person[selection] = 'rachmawati'
+
+console.log(person.nama);
