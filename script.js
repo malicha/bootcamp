d1fda4cabb1db0810568bd3b3f6fa72337550be3
malicha <maalicha.maalicha@gmail.com>
2019-01-14 21:42:07 +0700

    operator samadengan

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let x = 1;
 
-//operator lebih kecil lebih besar
-console.log(x > 0);
-console.log(x >= 1);
-console.log(x < 1);
-console.log(x <= 1);
+//sebuah pemenuhan kondisi/prosedur
+//type data dan value harus sama sama 
+// console.log(1 === 1);
 
-//operator persamaan
-console.log(x === 1);
-console.log(x !== 1);
+// //tipe data dengan value berbeda, tipe data string, value nya angka 1 outputnya false
+
+// console.log("1" === 1);
+
+//membandingkan
+console.log(1 == 1);
+
+//outputnya true karna biarpun tipe datanya brbeda dengan value nya, angkanya sama
+console.log("1" == 1);
