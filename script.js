3784fa0b6c03b51c78fb5d7657a6a473c9eef81d
malicha <maalicha.maalicha@gmail.com>
2019-01-16 12:31:46 +0700

    if else

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-//operator AND (&&) = semua nilai variable true, jika salah satu false maka outputnya false.
-// let highIncome = false;
-// let goodCreditsScore = true;
-// let malicha = true;
-// let eligibleForLoan = highIncome && goodCreditsScore && malicha;
 
-// console.log(eligibleForLoan);
-
-
-
-//operator OR (||) = wajib semua variable bernilai true, walaupun beberapa false
-let highIncome = false;
-let goodCreditsScore = true;
-let malicha = true;
-let eligibleForLoan = highIncome || goodCreditsScore || malicha;
-
-console.log('eligable', eligibleForLoan);
-
-
-//operator NOT (!) = kebalikan
-let aplication = !eligibleForLoan;
-console.log('aplication', aplication);
-
+function film(age){
+if(age > 18){
+    console.log ("OK")
+}
+else if(age < 18){
+    console.log ("NO")
+}
+}
+film(80);
